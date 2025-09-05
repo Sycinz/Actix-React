5332853e218edf174f5a71cc362a6898e4905965
Sycinz <mati.syta@protonmail.com>
2025-09-05 21:05:31 +0200

    Actix serving React

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_files::{NamedFile};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+
+    println!("Serving React on: http://localhost:8080");
+    HttpServer::new(|| {
+        App::new()
+            .service(actix_files::Files::new("/", "../frontend/dist").index_file("index.html"))
+    })
+    .bind(("localhost", 8080))?
+    .run()
+    .await
 }
