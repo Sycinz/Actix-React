443d776f94d38e3d7dcad4422712305e9254b324
Sycinz <mati.syta@protonmail.com>
2025-09-06 10:17:03 +0200

    Base App.jsx structure added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './scss/App.scss'
+import user_icon from './assets/user-icon.svg'
+import key_icon from './assets/key-icon.svg'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <form method="POST" action="login">
+        <div className="input-container">
+          <img src={user_icon} alt="User icon" type="image/svg+xml" className="user-icon" />
+          <input type="text" name="username" placeholder="Username" />
+        </div>
+        <div className="input-container">
+          <img src={key_icon} alt="Password icon" className="password-icon" />
+          <input type="password" name="password" placeholder="Password" />
+        </div>
+        <input type="submit" name="submit" value="LOGIN" />
+      </form>
     </>
   )
 }
